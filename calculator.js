66289dca00e8971baa2db00115316765d3e58062
Marie Fourriere <mariefourriere@gmail.com>
2020-11-16 17:38:06 +0100

    Problem with the = function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
-const arrayCalc = ["(", ")", '%', 'AC', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '0', '.', '=', '+'];
+const arrayCalc = ['(', ')', '%', 'AC', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '0', '.', '=', '+']
 
+console.log(arrayCalc)
 
 let button
 let line
@@ -8,24 +9,27 @@ document.querySelector('body');
 
 const contentCalc = document.createElement('div');
 contentCalc.className = 'contentCalc card'
+
 document.body.appendChild(contentCalc);
 
-const input = document.createElement('input');
-input.className = 'input is-primary'
-input.type = 'text'
-input.placeholder = '0'
-
 const results = document.createElement('div');
-results.className = 'results'
+results.className = 'displayResults'
+
+const inputResults = document.createElement('div');
+
 
 contentCalc.appendChild(results);
+results.appendChild(inputResults);
+
+
 
 for (let i = 0; i < arrayCalc.length; i++) {
     button = document.createElement('div')
     button.textContent = arrayCalc[i]
-    button.className = 'button'
+    button.className = 'button is-primary is-inverted has-text-weight-bold'
 
-    if (i <= 3){
+    //styling the buttons 
+    if (i <= 3) {
         button.className = 'button operators'
     }
 
@@ -33,20 +37,47 @@ for (let i = 0; i < arrayCalc.length; i++) {
         button.className = 'button operators'
     }
 
-    if (i === 18){
+    if (i === 18) {
         button.className = 'button is-primary operators'
     }
-    
+
     if (i % 4 === 0) {
         line = document.createElement('div')
         line.className = 'line'
     }
 
     
-    results.appendChild(input);
+    function resultsAll() {
+        return inputResults.textContent;
+        
+    } 
+
+    let calculate = arrayCalc[i];
+    switch (calculate) {
+
+        case 'AC':
+            button.addEventListener('click', function () {
+                inputResults.textContent = "";
+            });
+            break;
+
+        case '=':
+            button.addEventListener('click', function () {
+                inputResults.textContent = resultsAll()
+
+                console.log(resultsAll);
+            });
+            break;
+
+
+        default:
+            button.addEventListener('click', function () {
+                inputResults.textContent += arrayCalc[i];
+
+            });
+
+    };
     contentCalc.appendChild(line);
     line.appendChild(button);
+
 }
-
-
-
